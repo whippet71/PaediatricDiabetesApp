e85c02df8bd164b24dd62c41dd6ea310838c2564
John Morgan <christopher.morgan@ydh.nhs.uk>
2019-12-11 13:28:33 +0000

    Fixed base URL

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ if (workbox) {
   console.log(`Workbox is loaded`);
 
   workbox.precaching.precacheAndRoute(self.__precacheManifest);
-  workbox.routing.registerNavigationRoute('/index.html')
+  workbox.routing.registerNavigationRoute('/PaediatricDiabetesApp/index.html')
 } 
 else {
   console.log(`Workbox didn't load`);
